9b730cc43921197bb5f9a56930a4f77ad041bae1
Priyareshma <95283133+Priyareshma@users.noreply.github.com>
2021-12-02 17:50:14 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,8 +1,8 @@
 create table useraccount (id int primary key auto_increment not null,firstname varchar(100) not null,lastname varchar(100) not null,dob date not null,gender varchar(100) not null,email varchar(200) not null);
 desc useraccount;
-alter table useraccount modify column token double not null;
-alter table useraccount modify column otp double not null;
-alter table useraccount modify column createdtime time not null;
-alter table useraccount modify column otptime time not null;
-alter table useraccount modify column status int(10) not null;
+alter table useraccount add column token double not null;
+alter table useraccount add column otp double not null;
+alter table useraccount add column createdtime time not null;
+alter table useraccount add column otptime time not null;
+alter table useraccount add column status int(10) not null;
 select * from useraccount;
